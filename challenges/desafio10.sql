77259194413a2a7d68cd03df8229213c662cb875
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-05-20 14:35:31 -0300

    feat: Desafio 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,22 @@
--- Descomente e altere as linhas abaixo:
+CREATE TABLE SpotifyClone.cancoes_favoritas 
+(
+    pessoa_usuaria_id	INT NOT NULL,
+    cancao_id	INT NOT NULL,
+    CONSTRAINT PRIMARY KEY(pessoa_usuaria_id, cancao_id),
+	FOREIGN KEY (pessoa_usuaria_id) REFERENCES SpotifyClone.user (pessoa_usuaria_id),
+	FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancao (cancao_id)
+);
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('1', '3');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('1', '6');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('1', '10');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('2', '4');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('3', '1');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('3', '3');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('4', '7');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('4', '4');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('5', '10');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('5', '2');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('8', '4');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('9', '7');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_id) VALUES ('10', '3');
